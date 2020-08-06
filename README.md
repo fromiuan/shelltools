98bb9dc945edf1570e38aa42814a8c4cf7b9e5f0
fromiuan <root@ycandyz.com>
2020-08-06 14:36:56 +0800

    add mgr.sh file

--- README.md
+++ README.md
@@ -7,3 +7,4 @@
 1. `md_mysql.sh` 自动将Myslq数据库结构生成Md文档
 2. `camel_to_underscore.sh` 将驼峰法变成下划线命名法 
 3. `update_micro.py` 升级微服务(micro)并推送至阿里云
+4. `mgr.sh` 项目管理脚本
---
+++ mgr.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+# set project name
+PROJECT_NAME="readme.md"
+
+
+#mgr version
+VERSION="1.0"
+# rootFile
+rootFile=~
+
+function printHelp() {
+  echo "Usage: "
+  echo "  start.sh <arg>"
+  echo "    <arg> - one of 'start', 'upgrade', 'stop', 'restart', 'version', 'help'"
+  echo "      - 'help'       - echo help information "
+  echo "      - 'start'      - run start project "
+  echo "      - 'upgrade'    - run upgrade project "
+  echo "      - 'stop'       - run stop project "
+  echo "Taking all defaults:"
+  echo "  mgr.sh help"
+}
+
+
+function start() {
+ nohup ./$PROJECT_NAME &
+}
+
+function stop() {
+  ps -ef | grep $PROJECT_NAME | awk '{print $2}' | xargs kill
+}
+
+function upgrade() {
+  stop
+  mv $PROJECT_NAME $PROJECT_NAME"_bak"
+  cp ~/$PROJECT_NAME ./
+}
+
+function restart() {
+  stop
+  start
+}
+
+# run befor start command check
+function beforStart() {
+  if [ "${PROJECT_NAME}" == "" ]; then
+    echo "please set 'PROJECT_NAME'"
+    exit 1
+  fi
+  if [ ! -f  "./${PROJECT_NAME}" ]; then
+    echo "not find file ${PROJECT_NAME}"
+    exit
+  fi
+}
+
+# run befor upgrade command check
+function beforUpgrade() {
+  
+  beforStart
+  if [ ! -f "${rootFile}/${PROJECT_NAME}" ]; then
+    echo "not find file ${PROJECT_NAME} in '${rootFile}' dir"
+    exit
+  fi
+}
+
+arg=$1
+
+# run main
+if [ "${arg}" == "start" ]; then
+  beforStart
+  start
+elif [ "${arg}" == "upgrade" ]; then
+  beforUpgrade
+  upgrade
+elif [ "${arg}" == "stop" ]; then
+  stop
+elif [ "${arg}" == "restart" ]; then
+  restart
+elif [ "${arg}" == "version" ] || [ "${arg}" == "-v" ] || [ "${arg}" == "-version" ]; then
+  echo $VERSION
+elif [ "${arg}" == "hepl" ]  || [ "${arg}" == "-h" ]; then
+  printHelp
+else
+  printHelp
+  exit 1
+fi
